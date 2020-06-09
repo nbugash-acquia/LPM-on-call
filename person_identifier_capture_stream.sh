8dea1cec974d940248179a8dd675793b1c2450c6
Nico Bugash <nonico.bugash@acquia.com>
2020-06-09 14:29:16 -0400

    Update person_identifier_capture_stream.sh

--- person_identifier_capture_stream.sh
+++ person_identifier_capture_stream.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SHARD_ID=$1;\
-grep "shardId-${SHARD_ID}" /var/log/trucentric/awspersoncapture.log* | grep 'i:{' | awk '{ print $5 }' | cut -d , -f 2 | sort | uniq -c | sort -n
+grep "${SHARD_ID}" /var/log/trucentric/awspersoncapture.log* | grep 'i:{' | awk '{ print $5 }' | cut -d , -f 2 | sort | uniq -c | sort -n
